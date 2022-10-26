f08c24692fa6f58c707053dafee59eed59e87334
wenyi <winnieee.sun@gmail.com>
2022-10-25 21:23:00 -0700

    handle empty url input in posts.js

--- routes/api/v2/controllers/posts.js
+++ routes/api/v2/controllers/posts.js
@@ -6,19 +6,24 @@ import getURLPreview from "../utils/urlPreviews.js";
 
 //TODO: Add handlers here
 router.post("/", async (req, res) => {
-  try {
-    let newUserInput = new req.models.Post({
-      url: req.body.url,
-      description: req.body.description,
-      isFav:req.body.isFav,
-      created_date: Date(),
-    });
-    await newUserInput.save();
-    res.send({ status: "success" });
-  } catch (err) {
-    console.log("There is an error when saving user input: " + err);
-    res.status(500).json({ status: "error", error: error });
-  }
+    try {
+      if (Object.keys(req.body.url).length === 0) {
+        let error = new Error("Empty url input, please try again");
+        console.log(error);
+        throw error;
+      }
+      let newUserInput = new req.models.Post({
+        url: req.body.url,
+        description: req.body.description,
+        isFav: req.body.isFav,
+        created_date: Date(),
+      });
+      await newUserInput.save();
+      res.send({ status: "success" });
+    } catch (err) {
+      console.log("There is an error when saving user input: " + err);
+      res.status(500).json({ status: "error", error: err});
+    }
 });
 
 router.get("/", async (req, res) => {
@@ -48,7 +53,7 @@ router.get("/", async (req, res) => {
   );
   }catch(err){
        console.log("There is an error when posting all posts: " + err);
-       res.status(500).json({ status: "error", error: error });
+       res.status(500).json({ status: "error", error: err});
   }
   // console.log(previewboxes);
   res.send(resultsArr);
@@ -80,7 +85,7 @@ router.get("/fav", async (req, res) => {
     );
   } catch (err) {
     console.log("There is an error when posting favorates posts: " + err);
-    res.status(500).json({ status: "error", error: error });
+    res.status(500).json({ status: "error", error: err });
   }
   // console.log(previewboxes);
   res.send(resultsArr);
