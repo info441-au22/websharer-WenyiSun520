25224e06618ac897ac2a1cecd33bcc9319b6dd92
wenyi <winnieee.sun@gmail.com>
2022-11-01 20:05:38 -0700

    add microsoft express and auth initialization in app.js

--- app.js
+++ app.js
@@ -2,6 +2,20 @@ import express from 'express';
 import path from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
+import session from 'express-session';
+import msIdExpress from 'microsoft-identity-express';
+const appSettings = {
+  appCredenticals: {
+    clientId: "8c15ea7e-f9f6-4552-b3a1-fff19c5c024f",
+    tenantId: "f6b6dd5b-f02f-441a-99a0-162ac5060bd2",
+    clientSecret: "5IF8Q~i0G_c8Kfth4WVv_z-vX3XF_9Qtxv0jWa~L",
+  },
+  authRoutes: {
+    redirect: "https://wenyis.tech/redirect",
+    error: "/error",
+    unauthorized:"/unauthorized"
+  },
+};
 
 
 import apiv2Router from './routes/api/v2/apiv2.js'; // load apiv2 router
@@ -25,7 +39,16 @@ app.use((req,res,next)=>{
     next();
 
 })
-
+const msid = new msIdExpress.WebAppAuthClientBuilder(appSettings).build()
+app.use(msid.initialize());
+app.get('/signin', msid.signIn({postLoginRedirect:'/'}));
+app.get('/signout', msid.signOut({postLogoutRedirect: '/'}));
+app.get('/error', (req, res)=>{
+    res.status(500).send("Error: Server error")
+})
+app.get("/unauthorized", (req, res) => {
+  res.status(401).send("Error: Unauthorized");
+});
 app.use('/api/v2/', apiv2Router);
 
 export default app;
