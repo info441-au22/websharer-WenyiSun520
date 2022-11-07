388c63d17076590e17822244da3b9a2c93d9b789
wenyi <winnieee.sun@gmail.com>
2022-11-07 15:23:20 -0800

    debug signin err

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const appSettings = {
     clientSecret: "5IF8Q~i0G_c8Kfth4WVv_z-vX3XF_9Qtxv0jWa~L",
   },
   authRoutes: {
+    // redirect:"http://localhost:3000/redirect"
     redirect: "https://wenyis.tech/redirect",
     error: "/error",
     unauthorized: "/unauthorized",
